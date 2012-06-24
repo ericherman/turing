9df7596cb1285d92a7b608d06151b2eb16f2c141
Eric Herman <eric@freesa.org>
2012-06-24 16:15:16 +0200

    make verbose output optional

--- turing-machine.pl
+++ turing-machine.pl
@@ -4,15 +4,25 @@ use strict;
 use warnings;
 
 use YAML qw( LoadFile );
+use Getopt::Long;
+use Pod::Usage;
 
 use TuringMachine;
 
 # turn off output buffering
 $| = 1;
 
-my $file = "busy-beaver.yaml";
+my $verbose;
+my $help;
+GetOptions( "help" => \$help, "verbose" => \$verbose );
+
+my $file;
 if ( scalar @ARGV ) {
     $file = $ARGV[0];
+}
+
+if ( $help or not $file ) {
+    pod2usage(1);
 }
 
 my $yaml = LoadFile($file);
@@ -25,20 +35,28 @@ my $long_desc     = $yaml->{long_description};
 
 print "$name\n\n";
 print "$short_desc\n\n";
-print "$long_desc\n\n";
+if ($verbose) {
+    print "$long_desc\n\n";
+}
 
 my $turing_machine = new TuringMachine( $table, $initial_state );
 
-$turing_machine->set_bfunc(
-    sub {
-        my $turing_machine = shift;
-        print $turing_machine->peek(), "\n";
-    }
-);
+if ($verbose) {
+    $turing_machine->set_bfunc(
+        sub {
+            my $turing_machine = shift;
+            print $turing_machine->peek(), "\n";
+        }
+    );
+}
 
-print "\nexecution:\n";
+if ($verbose) {
+    print "\nexecution:\n";
+}
 $turing_machine->go();
-$turing_machine->bfunc();
+
+print "\nfinal state:\noperations: ", $turing_machine->peek(), "\n";
+
 
 print "\nfinal tape:\n";
 $turing_machine->for_each_tape_entry(
@@ -49,3 +67,37 @@ $turing_machine->for_each_tape_entry(
     }
 );
 
+__END__
+
+=head1 NAME
+
+turing-machine.pl - reads turing descriptions in YAML format
+
+=head1 SYNOPSIS
+
+turing-machien.pl [options] yamlfilename
+
+    Options:
+        -help        brief help message
+        -verbose     full printout of execution steps
+
+=head1 OPTIONS
+
+=over 8
+
+=item B<-help>
+
+Print a brief help message and exits.
+
+=item B<-verbose>
+
+prints more stuff.
+
+=back
+
+=head1 DESCRIPTION
+
+B<This program> will read the given input YAML file and execute the
+table assuming a blank, infinite strip.
+
+=cut
